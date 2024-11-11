65d42a5a12d9b0c66534412d4bf224f3fd587de0
music-miko <voilet856@gmail.com>
2024-11-11 06:55:53 +0530

    Update vars.py

--- Curse/vars.py
+++ Curse/vars.py
@@ -49,38 +49,38 @@ class Config:
     BDB_URI = config("BDB_URI", default=None)
     NO_LOAD = config("NO_LOAD", default="").split()
     PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="Lux_bot_support")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="Lux_bot_support")
+    SUPPORT_GROUP = config("SUPPORT_GROUP", default="nothing_bots_support")
+    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="The_hogwart")
     WORKERS = int(config("WORKERS", default=16))
     TIME_ZONE = config("TIME_ZONE", default='Asia/Kolkata')
     BOT_USERNAME = config("BOT_USERNAME", default=None)
     BOT_ID = config("BOT_ID", default=None)
     BOT_NAME = ""
-    owner_username = "hunter_karan"
+    owner_username = "its_damiann"
 
 class Development:
     """Development class for variables."""
     LOGGER = True
-    BOT_TOKEN = "5959061711:AAFvwYbdr5fI_kW3vajPcPQvjDAKQz3WT5I"
+    BOT_TOKEN = "7916855567:AAFWQ7BT7iA6FNJG61RhOkwZFu9YE5OykWI"
     API_ID = 9552179  # Your APP_ID from Telegram
     API_HASH = "fa6e0313afd8259094486d3256242102"  # Your APP_HASH from Telegram
     OWNER_ID = 6965147961  # Your telegram user id default to mine
     MESSAGE_DUMP = -1001646232965  # Your Private Group ID for logs
-    DEV_USERS = ["1643851457", "6848223695"]
+    DEV_USERS = ["7186437295"]
     SUDO_USERS = ["6848223695", "5458968679"]
     WHITELIST_USERS = ["7185106962"]
-    DB_URI = "mongodb+srv://abdulrahaman001:databaseKomi@komi.mosqcfj.mongodb.net/?retryWrites=true&w=majority&appName=KOmi"  # Your mongo DB URI
+    DB_URI = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Your mongo DB URI
     DB_NAME = "Curse"  # Your DB name
     NO_LOAD = []
     GENIUS_API_TOKEN = "gIgMyTXuwJoY9VCPNwKdb_RUOA_9mCMmRlbrrdODmNvcpslww_2RIbbWOB8YdBW9"
     RMBG_API = ""
     PREFIX_HANDLER = ["!", ".", "Komi ", "komi "]
-    SUPPORT_GROUP = "Lux_bot_support"
-    SUPPORT_CHANNEL = "Lux_bot_support"
+    SUPPORT_GROUP = "nothing_bots_support"
+    SUPPORT_CHANNEL = "The_Hogwart"
     VERSION = "3.O"
     TIME_ZONE = 'Asia/Kolkata'
-    BDB_URI = "mongodb+srv://safwandigi:Fazal2002@test.kzjdjnu.mongodb.net/?retryWrites=true&w=majority&appName=test"
+    BDB_URI = "mongodb+srv://orewauzumaki:orewauzumaki@cluster0.bmhengh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     WORKERS = 8
     BOT_USERNAME = "Komi_RoXbot"
-    BOT_ID = "5959061711"
+    BOT_ID = "7916855567"
     AuDD_API = ""
